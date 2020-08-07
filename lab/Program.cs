8687ea5b042c9c430206e0f9d83281ac72ce35af
mykelgozie <mykelgozie@gmail.com>
2020-08-07 23:34:14 +0100

    firsts

---
+++ lab/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        int  num = 200;
+
+            string check = "";
+
+
+            if ( num < 0)
+            {
+
+              Console.WriteLine("invalid");
+
+
+            }
+
+            for (int i = 1; i <= num; i++)
+            {
+
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+
+                    check = check + "\nfizzbuzz";
+
+
+                }
+                else if (i % 3 == 0)
+                {
+
+
+                    check = check + "\nfizz";
+
+                }
+                else if (i % 5 == 0)
+                {
+
+                    check = check + "\nbuzz";
+
+                }
+                else if (i % 3 != 0 || i % 5 != 0)
+                {
+
+
+                    check = check + "\n" + i;
+
+                }
+
+
+
+            }
+
+
+            Console.WriteLine(check);
+
+
+
+
+
+
+        }
+    }
+}
